1bdddf5feae0ff6266e8c0b646ccca1285d4f8d8
Agherrabi <agherrabisaid@gmail.com>
2020-12-12 23:33:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const typedTxtspan = document.querySelector(".text-typing")
+
+const arrayTxt = ["Simple","beautifull","hard","one ."]
+const typingDelay = 200;
+const erasingDelay = 100;
+const newtXTDelay = 2000;
+let txtarrayindex = 0;
+let charindex = 0;
+
+function type(){
+    
+if(charindex < arrayTxt[txtarrayindex].length){
+    typedTxtspan.textContent += arrayTxt[txtarrayindex].charAt(charindex);
+    charindex++;
+    setTimeout(type,typingDelay);
+    
+}else{
+    setTimeout(erasi,newtXTDelay)
+
+}
+
+}
+function erasi() {
+    if(charindex > 0){
+        typedTxtspan.textContent = arrayTxt[txtarrayindex].substring(0,charindex-1);
+        charindex--;
+        setTimeout(erasi,erasingDelay);
+        
+    }
+    else{
+        txtarrayindex++;
+        if(txtarrayindex >= arrayTxt.length) txtarrayindex=0;
+        setTimeout(type,typingDelay+1100)
+    }
+}
+
+
+type();
